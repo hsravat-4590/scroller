ea42c675a10029399d81c6df199322f911ed1869
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-08-21 12:25:41 +0100

    Initial Commit

---
+++ webserver/src/test/kotlin/com/hanzalahravat/scroller/webserver/WebserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hanzalahravat.scroller.webserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
